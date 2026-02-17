e15a4af04c6f1b8aeda0c5b259c2c5515a2b0b97
Lenard To <lenardbto@gmail.com>
2026-02-17 16:02:05 +0800

    Fixed lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Navigation from '@/components/Navigation';
 import Hero from '@/components/Hero';
 import Destinations from '@/components/Destinations';
-import Experiences from '@/components/Experiences';
 import About from '@/components/About';
 import Testimonials from '@/components/Testimonials';
-import Journal from '@/components/Journal';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
